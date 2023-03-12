2fcfdfe0e47a5bfd3c4836c7d4f6826fe0a3f977
go2collage <91822422+go2collage@users.noreply.github.com>
2023-03-12 14:46:12 +0530

    Add files via upload

---
+++ BP_P22.py
@@ -0,0 +1,14 @@
+# Basic Python Program
+
+# Sum all items in a list
+
+list1 = [10, 20, 30, 40, 50]
+i = 0
+sum1 = 0
+
+for x in list1:
+    sum1 = list1[i] + sum1      # 10 + 20 + 30 + 40 + 50 = 150
+    i = i + 1
+
+print("Sum of list1 all items is: ", sum1)
+
